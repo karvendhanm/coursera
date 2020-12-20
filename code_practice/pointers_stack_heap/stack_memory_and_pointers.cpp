42ed3e9de4cdb42d3edc7dfec4f193bd798f496d
Karvendhan M <karvsmech@gmail.com>
2020-12-20 13:34:57 +0530

    memory allocation is stack WSL vs cloud9

--- code_practice/pointers_stack_heap/stack_memory_and_pointers.cpp
+++ code_practice/pointers_stack_heap/stack_memory_and_pointers.cpp
@@ -5,17 +5,21 @@ using std::endl;
 
 int moo() {
 	int j = 5;
+	int k = 15;
 	cout << "the value of j in moo is(expected value is 5): " << j << endl;
 	cout << "the address of j in moo is : " << &j << endl;
+	cout << "the address of k in moo is : " << &k << endl;
 	return 0 ;
 }
 
 
 int foo(){
 	int j = 4;
+	int k = 9;
 	moo();
 	cout << "the value of j in foo is(expected value is 4): " << j << endl;
 	cout << "the address of j in foo is : " << &j << endl;
+	cout << "the address of k in foo is : " << &k << endl;
 	return 0;
 }
 
