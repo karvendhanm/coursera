19a458d81780ded99f664e0a7016788884f1f2ce
Karvendhan M <karvsmech@gmail.com>
2020-12-20 13:10:14 +0530

    stack memory allocation in WSL(windows subsystem in Linux)

---
+++ code_practice/pointers_stack_heap/stack_memory_and_pointers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int moo() {
+	int j = 5;
+	cout << "the value of j in moo is(expected value is 5): " << j << endl;
+	cout << "the address of j in moo is : " << &j << endl;
+	return 0 ;
+}
+
+
+int foo(){
+	int j = 4;
+	moo();
+	cout << "the value of j in foo is(expected value is 4): " << j << endl;
+	cout << "the address of j in foo is : " << &j << endl;
+	return 0;
+}
+
+
+
+int main() {
+	int j = 3;
+	int k = 4;
+	foo();
+	cout << "the value of j in main is(expected value is 3): " << j << endl;
+	cout << "the address of j in main is : " << &j << endl;
+	cout << "the address of k in main is : " << &k << endl;
+	return 0;
+}
+
+
