d8996ff38e69b599597d9b3e69d0817ab8674e2a
Karvendhan M <karvsmech@gmail.com>
2020-12-21 10:52:22 +0530

    double pointers triple pointers and referencing

---
+++ code_practice/pointers_stack_heap/heap_stack_puzzle_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+
+	int* x = new int;
+	*x = 223;
+	cout << "the expected value of *x is 223: " << *x << endl;
+	cout << "the expected value of x is : " << x << endl;
+	
+	int** y = &x;
+	**y = 445;
+	cout << "the expected value of *x is 445: " << *x << endl;
+
+	int &p = *x;
+	int* &q = x;
+
+	cout << "the value of p is (445): " << p << endl;
+	cout << "the value of *q is " << *q << endl;
+	cout << "the value of q is " << q << endl;
+
+	int*** z = &y;
+	int*** &r = z;
+	
+	cout << "the value of x is: " << x << endl;
+	cout << "the address of x is: " << &x << endl;
+	cout << "the value of y is: " << y << endl;
+	cout << "the address of y is: " << &y << endl;
+	cout << "the value of z is: " << z << endl;
+	cout << "the address of z is: " << &z << endl;
+	return 0;
+}
