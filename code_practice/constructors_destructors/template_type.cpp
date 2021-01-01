7658bb19688bf68a95a1b935b958b3b8ee0caf73
Karvendhan M <karvsmech@gmail.com>
2021-01-01 14:18:45 +0530

    dynamically growing arrays - vectors

--- code_practice/constructors_destructors/template_type.cpp
+++ code_practice/constructors_destructors/template_type.cpp
@@ -16,16 +16,27 @@ int main() {
 	}
 
 	std::vector<Cube> Cube_list;
+	cout << "\n Here comes c1 ***" << endl;
 	Cube c1(5);
+	cout << "\n Here comes c2 ***" << endl;
 	Cube c2(10);
+	cout << "\n Here comes c3 ***" << endl;
 	Cube c3(15);
+	cout << "\n Here comes c1 pushback ***" << endl;
 	Cube_list.push_back(c1);
+	cout << "\n Here comes c2 pushback ***" << endl;
 	Cube_list.push_back(c2);
+	cout << "\n Here comes c3 pushback ***" << endl;
 	Cube_list.push_back(c3);
 
 	for(Cube s: Cube_list) {
 		cout <<  "the volume of the cube is: " << s.getVolume() << endl;
 	}
 	
+	for(int i=0; i< int(Cube_list.size()); i++) {
+		cout << "the value is: " << Cube_list[i].getVolume() << endl;
+		cout << "the address is: " << &Cube_list[i] << endl;
+	}
+
 	return 0;
 }
