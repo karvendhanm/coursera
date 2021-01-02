07cdef41a6e3922d92b8d19d41e9bdaf25c5c04d
Karvendhan M <karvsmech@gmail.com>
2021-01-02 17:07:05 +0530

    static variables in a class

---
+++ code_practice/tower_of_hanoi/GfG.cpp
@@ -0,0 +1,9 @@
+#include "GfG.h"
+#include <iostream>
+
+int GfG::i = 2;
+
+GfG::GfG() {
+	std::cout << "invoked the default class conctructor" << std::endl;
+}
+
---
+++ code_practice/tower_of_hanoi/static_class_variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "GfG.h"
+
+int main() {
+	GfG obj1;
+	GfG obj2;
+
+	std::cout << "object1 is: " << obj1.i << std::endl;
+	std::cout << "object2 is: " << obj2.i << std::endl;
+
+	return 0;
+}
