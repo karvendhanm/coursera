36568856440e892dfecba39072a0bd12f167d153
Karvendhan M <karvsmech@gmail.com>
2021-01-04 08:50:50 +0530

    HSLAPixel revision

--- code_practice/tower_of_hanoi/uiuc/HSLAPixel.h
+++ code_practice/tower_of_hanoi/uiuc/HSLAPixel.h
@@ -1,22 +1,21 @@
 #pragma once
-#include <iostream>
+
 
 namespace uiuc {
 	class HSLAPixel {
 		public:
+			HSLAPixel();
+			HSLAPixel(double hue, double saturation, double luminance);	
+			HSLAPixel(double hue, double saturation, double luminance, double alpha);
+			
+			static HSLAPixel BLUE;
+			static HSLAPixel YELLOW;
+			static HSLAPixel ORANGE;
+			static HSLAPixel PURPLE;
+
 			double h;
 			double s;
 			double l;
-			double a;
-
-			HSLAPixel();
-			HSLAPixel(double hue, double saturation, double luminance);
-			HSLAPixel(double hue, double saturation, double luminance, double alpha);			
-
-			static HSLAPixel BLUE;
-			static HSLAPixel ORANGE;
-			static HSLAPixel YELLOW;
-			static HSLAPixel PURPLE;
-		
-	};
+			double a;	
+		};
 }
