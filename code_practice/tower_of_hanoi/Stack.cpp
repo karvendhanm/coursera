a21b16403d404204495a37587b915da17954cb31
Karvendhan M <karvsmech@gmail.com>
2021-01-10 17:12:56 +0530

    Stack fully formed

--- code_practice/tower_of_hanoi/Stack.cpp
+++ code_practice/tower_of_hanoi/Stack.cpp
@@ -1,8 +1,18 @@
 #include "Stack.h"
 #include <iostream>
+#include <exception>
 
 void Stack::push_back(const Cube & cube) {
-	std::cout << "the memory address of Cube object cube is: " << &cube << std::endl;
+	std::cout << "the memory address of Cube object cube inside Stack.cpp push_back is: " << &cube << std::endl;
+
+	if(size() > 0 && cube.getLength() > peek_top().getLength())	{
+		std::cerr << "A Larger cube cannot be placed on top of a smaller cube." << std::endl;
+		std::cerr << "Tried to add Cube(lenght=" << cube.getLength() << ")" << std::endl;
+		std::cerr << " Current stack: " << *this << std::endl;		
+
+		throw std::runtime_error("A Larger cube cannot sit on top of a smaller cube."); 
+	}
+
 	cubes_.push_back(cube);
 }
 
@@ -15,3 +25,24 @@ void Stack::print_memory_address() const {
 		std::cout << "At index: " << i << " the memory address is: " << &cubes_[i] << std::endl;
 	}
 }
+
+Cube Stack::remove_top() {
+	Cube cube = this->peek_top();
+	std::cout << "The address of the cube inside Stack.cpp remove_top is: " << &cube << std::endl;
+	cubes_.pop_back();
+	return cube;		
+}
+
+Cube & Stack::peek_top() {
+	unsigned size = this->size();
+	return cubes_[size-1];	
+}
+
+std::ostream & operator<<(std::ostream & os, const Stack & stack) {
+	for(unsigned i = 0; i < stack.size(); i++) {
+		os << stack.cubes_[i].getLength() << " ";
+	}	
+os << std::endl;
+return os;
+}
+
--- code_practice/tower_of_hanoi/Stack.h
+++ code_practice/tower_of_hanoi/Stack.h
@@ -1,5 +1,7 @@
+#pragma once
 #include <vector>
 #include "uiuc/Cube.h" 
+#include <ostream>
 
 using uiuc::Cube;
 
@@ -8,6 +10,10 @@ class Stack {
 	void push_back(const Cube & cube);
 	unsigned size() const;
 	void print_memory_address() const;
+	Cube remove_top();
+	Cube & peek_top();
+
+	friend std::ostream & operator<<(std::ostream & os, const Stack & stack);
 	
   private:
     	std::vector<Cube> cubes_;
