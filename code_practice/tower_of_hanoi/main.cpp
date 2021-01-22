78c12e41dd2a66e7251bc5f371f93b6afff7d6c4
Karvendhan M <karvsmech@gmail.com>
2021-01-22 07:17:33 +0530

    concatenating vectors, 3 vectors example

--- code_practice/tower_of_hanoi/main.cpp
+++ code_practice/tower_of_hanoi/main.cpp
@@ -34,6 +34,25 @@ int main() {
 	}
 	// Sorting vectors tangent ends.
 
+	// concatenating vectors - Tangent
+	std::vector<int> v1{4, 6, 9, 2, 11};
+	std::vector<int> v2{9, 12, 13, 15, 19};
+	std::vector<int> v3{19, 22, 25, 39, 34, 12};
+
+	std::vector<int> v4(v1.size() + v2.size());
+	merge(v1.begin(), v1.end(), v2.begin(), v2.end(), v4.begin());
+
+	std::vector<int> v5(v3.size() + v4.size());
+	merge(v3.begin(), v3.end(), v4.begin(), v4.end(), v5.begin());
+
+	//sort(v5.begin(), v5.end());
+	sort(v5.begin(), v5.end(), std::greater<int>());
+
+	for(unsigned i = 0; i < v5.size(); i++) {
+		cout << "the sorted number in v5 is: " << v5[i] << endl;
+	}
+	// Concatenating vectors tangent ends.
+
 	return 0;
 }
 
