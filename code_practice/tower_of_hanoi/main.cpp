19174b3584efca1b70bc16fdbedc2f617732390f
Karvendhan M <karvsmech@gmail.com>
2021-01-22 06:05:54 +0530

    sorting vectors using sort function

--- code_practice/tower_of_hanoi/main.cpp
+++ code_practice/tower_of_hanoi/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include "Stack.h"
 #include "Game.h"
+#include <bits/stdc++.h>
 
 using uiuc::Cube;
 using std::cout;
@@ -10,9 +11,11 @@ int main() {
 	Stack stack_;
 	Cube c_(25, HSLAPixel::BLUE);
 	Cube c1_(15, HSLAPixel::PURPLE);
+	Cube c2_(5, HSLAPixel::ORANGE);
 
 	stack_.push_back(c_);
 	stack_.push_back(c1_);
+	stack_.push_back(c2_);
 
 	std::vector<double> temp = stack_.getAllCubeLengths();
 	unsigned stackSize_ = temp.size();
@@ -21,6 +24,16 @@ int main() {
 	}
 
 	cout << "this is working" << endl;
+
+	// Sorting vectors - Tangent
+	std::vector<int> v{1, 5, 8, 9, 6, 7, 3, 4, 2, 0};
+	sort(v.begin(), v.end());
+
+	for(unsigned i = 0; i < v.size(); i++) {
+		cout << "the sorted number is: " << v[i] << endl;
+	}
+	// Sorting vectors tangent ends.
+
 	return 0;
 }
 
